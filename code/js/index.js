eb0501a6a1b1336cc01a1465d3efe917e2263636
WebWorm3 <Ivan2002dav@gmail.com>
2018-04-10 17:12:35 +0300

    index.js fix

--- code/js/index.js
+++ code/js/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <h2>Здесь может быть любой текст</h2>, 
+  <h2>Hello world</h2>, 
   document.getElementById('fieldToShow')
 );
